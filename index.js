4deed98dda4d0522d5210b2bc24f807f249908a1
Nunzio Tocci <nunzio@tocci.org>
2016-11-08 15:35:42 -0600

    update to letterbox 2.0.0

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const letterbox = require('letterbox');
+const postcard = require('postcard');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -6,15 +7,19 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindows = [];
 
 app.on('ready', function () {
-	letterbox.init('letterbox-example', function (envelopePort) {
+	postcard.init('letterbox-example', function (postgresPort) {
 		var curWindow = new BrowserWindow({
 			'width': 1024,
 			'height': 768
 		});
 		mainWindows.push(curWindow);
+		letterbox.init('letterbox-example',
+				(process.platform == 'win32' ? '127.0.0.1' : '/tmp'), postgresPort,
+				function (envelopePort) {
 
-		curWindow.loadURL('http://127.0.0.1:' + envelopePort, {
-			'extraHeaders': 'pragma: no-cache\n'
+			curWindow.loadURL('http://127.0.0.1:' + envelopePort, {
+				'extraHeaders': 'pragma: no-cache\n'
+			});
 		});
 
 		// Emitted when the window is closed.
@@ -26,4 +31,5 @@ app.on('ready', function () {
 
 app.on('quit', function() {
 	letterbox.quit();
+	postcard.quit();
 });
